d65cbeadc21916c7b2bd62a1cd710825b36bcdba
Shubham1069 <149653782+Shubham1069@users.noreply.github.com>
2024-03-27 11:37:56 +0530

    Update DockerJenkinsApplication.java

--- src/main/java/com/example/dockerjenkins/DockerJenkinsApplication.java
+++ src/main/java/com/example/dockerjenkins/DockerJenkinsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+	// This is the main application
 public class DockerJenkinsApplication {
 
 	public static void main(String[] args) {
